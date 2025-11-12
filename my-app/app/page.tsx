c07103f807bcc7ef9b3a0a05ba900f952507354b
Edwin Lin <burgerlivesmatter@gmail.com>
2025-11-11 20:59:50 -0500

    Update page.tsx

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,10 +1,16 @@
-import Image from "next/image";
+export default async function Home() {
+  const tempData = await fetch("http://127.0.0.1:8000/ev-incentives");
+  const finalData = await tempData.json();
 
-export default function Home() {
+
+  
+  const xAxis = finalData.map((temp) => temp.x);
+  const yAxis = finalData.map((temp) => temp.y);
+
   return (
     <div>
-      <h1 className='font-bold'>WE ARE WINNING THIS!</h1>
-    </div>
+      <h1 className="font-bold">WE ARE WINNING THIS!</h1>
       
+    </div>
   );
 }
